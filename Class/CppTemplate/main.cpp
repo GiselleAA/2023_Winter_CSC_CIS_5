4e40883bb86c7e1a0612b6a2b8fd9afefc52bc3b
GiselleAA <gavila2025@gmail.com>
2023-01-13 02:46:10 -0800

    Signed-off-by: GiselleAA <gavila2025@gmail.com>

---
+++ Class/CppTemplate/main.cpp
@@ -0,0 +1,38 @@
+
+/* 
+ * File:   main.cpp
+ * Author: avila
+ * Created on January 13, 2023, 2:25 AM
+ * Purpose: Template used for all
+ *          future hmrk, Labs, Exams, projects                  
+ */
+
+//System Libraries
+#include <iostream> //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants and not Variables 
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    
+    //Clean up memory and files
+    
+    //Exit the Program
+    return 0;
+}
+
---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,42 @@
+
+/* 
+ * File:   main.cpp
+ * Author: avila
+ * Created on January 13, 2023, 2:25 AM
+ * Purpose: Hello World
+ *     First Program all programmers write
+ *     when testing their IDE for the first 
+ *     time. 
+ *     C: Programming Language by
+ *     Dennis Ritchie and Brian Kerningham           
+ */
+
+//System Libraries
+#include <iostream> //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants and not Variables 
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    cout << "Hello World" << endl;
+    //Clean up memory and files
+    
+    //Exit the Program
+    return 0;
+}
+
