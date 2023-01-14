bb7e026d5e97b342f128cd921647d4cb990f3b4f
GiselleAA <gavila2025@gmail.com>
2023-01-13 19:41:42 -0800

    Signed-off-by: GiselleAA <gavila2025@gmail.com>

---
+++ Code-E/Hello World - CodeE/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * Author: Giselle Avila
+ * Date:1/13/23
+ * Purpose: Hello World
+ *     First Program all programmers write
+ *     when testing their IDE for the first 
+ *     time. 
+ *     C: Programming Language by
+ *     Dennis Ritchie and Brian Kerningham           
+*/
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Constants like PI, e, Gravity, Conversions, 2D array size only!
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Set Random Number Seed Here
+    
+    //Declare all Variables Here
+    
+    string origHi = "Hello World!";
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    cout << '\n';
+    cout << "   " << origHi << "  " << endl;
+    cout << '\n';
+
+    //Exit
+    return 0;
+}
